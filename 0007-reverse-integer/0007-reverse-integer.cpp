bf04cad9436f09195fe94b352f49e15c1b2906b0
RonitJariwala <143431555+RonitJariwala@users.noreply.github.com>
2025-12-22 18:17:58 +0530

    Time: 0 ms (100%), Space: 8.5 MB (53.8%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int reverse(int x) {
+        long long rev=0,digit;
+        while(x!=0){
+            digit=x%10;
+            rev=(rev*10)+digit;
+            x=x/10;
+        }
+        if (rev > INT_MAX || rev < INT_MIN) return 0;
+        return (int)rev;
+    }
+};
