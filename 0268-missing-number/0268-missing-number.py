cfe048774354331c604bad615e216b864c284c58
RonitJariwala <143431555+RonitJariwala@users.noreply.github.com>
2026-01-14 12:51:10 +0530

    Time: 0 ms (100%), Space: 20.5 MB (9.79%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.py
@@ -0,0 +1,7 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        n=len(nums)
+        total_sum=n*(n+1)//2
+        actual=sum(nums)
+        return total_sum-actual
+        
