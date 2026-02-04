6f61602874ea2bd038d011419e31b8eedd118fe2
RonitJariwala <143431555+RonitJariwala@users.noreply.github.com>
2026-02-04 14:09:31 +0530

    Time: 7 ms (64.9%), Space: 20 MB (14.39%) - LeetHub

---
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        n=len(nums)
+        ans=float('-inf')
+        pre,suf=1,1
+        for i in range(n):
+            if pre==0:
+                pre=1
+            if suf==0:
+                suf=1
+            pre*=nums[i]
+            suf*=nums[n-i-1]
+            ans=max(pre,suf,ans)
+        return ans
+        
