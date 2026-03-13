8a1ffc5549b81400a19ad8b006a2f41eafebd56c
RonitJariwala <143431555+RonitJariwala@users.noreply.github.com>
2026-03-13 18:02:19 +0530

    Time: 7 ms (60.43%), Space: 20.3 MB (60.78%) - LeetHub

--- 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.py
+++ 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.py
@@ -2,11 +2,12 @@ class Solution:
     def largestOddNumber(self, s: str) -> str:
         ind=-1
         i=0
+        #find end index and then traverse from start to end eliminating zeroes
         for i in range(len(s)-1,-1,-1):
             if (int(s[i])%2)==1:
                 ind=i
                 break
-            # skip leading zeroes edge case
+            # skip leading zeroes 
         i=0
         while i<=ind and s[i]=='0':
                 i+=1
