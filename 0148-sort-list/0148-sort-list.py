f1d6a9d412f65cd3964677b6872dce964e1963f0
RonitJariwala <143431555+RonitJariwala@users.noreply.github.com>
2026-04-03 14:19:56 +0530

    Time: 189 ms (30.11%), Space: 40.9 MB (25.84%) - LeetHub

---
+++ 0148-sort-list/0148-sort-list.py
@@ -0,0 +1,45 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+
+    def findMiddle(self,head):
+        if not head or not head.next:
+            return head
+        slow=head
+        fast=head.next
+        while fast and fast.next:
+            slow=slow.next
+            fast=fast.next.next
+        return slow
+        
+    def mergeTwoList(self,list1,list2):
+        dummy=ListNode(-1)
+        temp=dummy
+        while list1 and list2:
+            if list1.val<=list2.val:
+                temp.next=list1
+                list1=list1.next
+            else:
+                temp.next=list2
+                list2=list2.next
+            temp=temp.next
+        if list1:
+            temp.next=list1
+        else:
+            temp.next=list2
+        return dummy.next
+
+
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+        middle=self.findMiddle(head)
+        right=middle.next
+        middle.next=None
+        left=head
+        left=self.sortList(left)
+        right=self.sortList(right)
+        return self.mergeTwoList(left,right)
