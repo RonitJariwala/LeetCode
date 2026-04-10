6e93191e740c29ad635f9a659dc4c8241ce53c9a
RonitJariwala <143431555+RonitJariwala@users.noreply.github.com>
2026-04-10 18:10:05 +0530

    Time: 0 ms (100%), Space: 19.5 MB (25.54%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
@@ -1,4 +1,31 @@
 class Solution:
+    INT_MIN = -2**31
+    INT_MAX = 2**31 - 1
+
+
+    def helper(self,s,i,num,sign):
+        if i>=len(s) or not s[i].isdigit():
+            return num*sign
+        num=num*10+int(s[i])
+        if sign*num<=self.INT_MIN:
+            return self.INT_MIN
+        if sign*num>=self.INT_MAX:
+            return self.INT_MAX
+        return self.helper(s,i+1,num,sign)
+
+    def myAtoi(self, s: str) -> int:
+        i=0
+        while i<len(s) and s[i]==' ':
+            i+=1
+        sign=1
+        if i<len(s) and (s[i]=='+' or s[i]=='-'):
+            sign=-1 if s[i]=='-' else 1
+            i+=1
+        return self.helper(s,i,0,sign)
+
+
+
+    ''' Recursive extra stack O(N)
     def myAtoi(self, s: str) -> int:
         INT_MIN=-2**31
         INT_MAX=2**31-1
@@ -21,3 +48,4 @@ class Solution:
                 return INT_MIN
             i+=1
         return num*sign
+        '''
