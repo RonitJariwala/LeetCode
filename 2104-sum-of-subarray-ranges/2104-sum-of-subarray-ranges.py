fa605aeb0ec3f26462ec083d140f84a1ad26c931
RonitJariwala <143431555+RonitJariwala@users.noreply.github.com>
2026-06-30 12:45:20 +0530

    Time: 24 ms (44.87%), Space: 19.6 MB (19.78%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.py
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.py
@@ -1,11 +1,77 @@
 class Solution:
-    def subArrayRanges(self, nums: List[int]) -> int:
-        n=len(nums)
-        total=0
+    def findNSE(self,arr):
+        n=len(arr)
+        ans=[0]*n
+        st=[]
+        for i in range(n-1,-1,-1):
+            curEl=arr[i]
+            while st and arr[st[-1]]>=curEl:
+                st.pop()
+            ans[i]=st[-1] if st else n
+            st.append(i)
+        return ans
+
+    def findNGE(self,arr):
+        n=len(arr)
+        ans=[0]*n
+        st=[]
+        for i in range(n-1,-1,-1):
+            curEl=arr[i]
+            while st and arr[st[-1]]<=curEl:
+                st.pop()
+            ans[i]=st[-1] if st else n
+            st.append(i)
+        return ans
+    
+    def findPSE(self,arr):
+        n=len(arr)
+        ans=[0]*n
+        st=[]
         for i in range(n):
-            smallest,largest=nums[i],nums[i]
-            for j in range(i+1,n):
-                largest=max(nums[j],largest)
-                smallest=min(nums[j],smallest)
-                total+=(largest-smallest)
-        return total
+            curEl=arr[i]
+            while st and arr[st[-1]]>curEl:
+                st.pop()
+            ans[i]=st[-1] if st else -1
+            st.append(i)
+        return ans
+    
+    def findPGE(self,arr):
+        n=len(arr)
+        ans=[0]*n
+        st=[]
+        for i in range(n):
+            curEl=arr[i]
+            while st and arr[st[-1]]<curEl:
+                st.pop()
+            ans[i]=st[-1] if st else -1
+            st.append(i)
+        return ans
+    
+    def subarrayMins(self,arr): #use nse and pse in this
+        nse=self.findNSE(arr)
+        pse=self.findPSE(arr)
+        n=len(arr)
+        total_sum=0
+        for i in range(n):
+            left=i-pse[i]
+            right=nse[i]-i
+            freq=left*right*1
+            val=(freq*arr[i]*1)
+            total_sum+=val
+        return total_sum
+    
+    def subarrayMaxs(self,arr):  #use nge and pge in this
+        nge=self.findNGE(arr)
+        pge=self.findPGE(arr)
+        n=len(arr)
+        total_sum=0
+        for i in range(n):
+            left=i-pge[i]
+            right=nge[i]-i
+            freq=left*right*1
+            val=(freq*arr[i]*1)
+            total_sum+=val
+        return total_sum
+
+    def subArrayRanges(self,nums):
+        return (self.subarrayMaxs(nums)-self.subarrayMins(nums))
